7a3587348dd4335aaaab16f1da7f6f05eb711639
ElizabethRay94 <elizabeth.ray@salecycle.com>
2026-01-15 16:11:48 +0000

    er-training/add further intergration tests

--- integration/suites/banned_test.go
+++ integration/suites/banned_test.go
@@ -1,6 +1,7 @@
 package suites
 
 import (
+	"fmt"
 	helpers "localstack-demo/integration"
 	"localstack-demo/src/localstack"
 	"testing"
@@ -33,11 +34,55 @@ func TestBannedPlayerMatches(t *testing.T) {
 
 // And now we can simulate a bunch of scenarios that dynamoDB could respond in!
 // And we can confirm our service handles it all correctly and produces the correct output!
+
+// If there's a DONKEY in the system and we ask for SHREK, what happens?
 func TestBannedPlayerDoesNotMatch(t *testing.T) {
+	// setting up a dynamoService to have access to dynamodb
+	dynamoService, _ := localstack.NewDynamoClient()
+
+	// adding "DONKEY" into the banned players table
+	helpers.AddBannedPlayer("DONKEY", dynamoService)
+
+	// remove "DONKEY" from banned players table - once everything else is finished
+	defer helpers.RemoveBannedPlayer("DONKEY", dynamoService)
+
+	// run the banned name check and store the result in output
+	output := helpers.RunBannedCheck("SHREK")
+
+	// assert that the output will be false (SHREK is not in the banned players table)
+	assert.Equal(t, output, "false")
 }
 
+// If theres no players in the system, what happens when the app runs?
 func TestNoPlayers(t *testing.T) {
+	output := helpers.RunBannedCheck("SHREK")
+
+	assert.Equal(t, output, "false")
 }
 
+// If multiple banned players are in the system, what happens when the app tries to query each of them?
+// And then query a player that then doesnt exist while it's in this state?
 func TestMultipleBannedPlayers(t *testing.T) {
+	dynamoService, _ := localstack.NewDynamoClient()
+
+	helpers.AddBannedPlayer("DONKEY", dynamoService)
+	defer helpers.RemoveBannedPlayer("DONKEY", dynamoService)
+
+	helpers.AddBannedPlayer("SHREK", dynamoService)
+	defer helpers.RemoveBannedPlayer("SHREK", dynamoService)
+
+	helpers.AddBannedPlayer("FIONA", dynamoService)
+	defer helpers.RemoveBannedPlayer("FIONA", dynamoService)
+
+	outputOne := helpers.RunBannedCheck("DONKEY")
+	outputTwo := helpers.RunBannedCheck("SHREK")
+	outputThree := helpers.RunBannedCheck("FIONA")
+	outputFour := helpers.RunBannedCheck("LORDFARQUAAD")
+	fmt.Println("im different")
+
+	// assert that the output will be true for all except LORDFARQUAAD which is not in the banned players table
+	assert.Equal(t, outputOne, "true")
+	assert.Equal(t, outputTwo, "true")
+	assert.Equal(t, outputThree, "true")
+	assert.Equal(t, outputFour, "false")
 }
